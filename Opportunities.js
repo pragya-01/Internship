8338e22419bde614d2d06eda3df602cf9e1e0306
Pragya Agrawal <56079032+pragya-01@users.noreply.github.com>
2021-06-19 07:02:29 +0530

    Update and rename App.js to Opportunities.js

--- Opportunities.js
+++ Opportunities.js
@@ -1,39 +1,39 @@
-import './App.css';
+import './Opportunities.css';
 import { GrNetwork} from "react-icons/gr";
 import {FaHandshake} from "react-icons/fa";
 import {GoListUnordered} from "react-icons/go";
 import React, {Component} from 'react';
 
- export class App extends Component{
+ export class Opportunities extends Component{
    render() {
    return (
-   <div className="oppurtunities">
-             <div className="oppurtunitiesContainer">
-             <GrNetwork className="iconOppurtunitiesContainer"/>
-                  <p className="titleOppurtunitiesContainer">
+    <div className="opportunities">
+             <div className="opportunitiesContainer">
+             <GrNetwork className="iconOpportunitiesContainer"/>
+                  <p className="titleOpportunitiesContainer">
                    <b>Network Globally</b>
                  </p>
-                    <p className="descriptionOppurtunitiesContainer">
+                    <p className="descriptionOpportunitiesContainer">
                     Describe a key benefit that your product provides for
                     your customer- and explain the impact it can have.
                     </p>
              </div>
-             <div className="oppurtunitiesContainer">
-             <GoListUnordered className="iconOppurtunitiesContainer"/>
-                    <p className="titleOppurtunitiesContainer">
+             <div className="opportunitiesContainer">
+             <GoListUnordered className="iconOpportunitiesContainer"/>
+                    <p className="titleOpportunitiesContainer">
                     <b>Enter new markets and buisness segments</b>
                     </p>
-                    <p className="descriptionOppurtunitiesContainer">
+                    <p className="descriptionOpportunitiesContainer">
                     Describe a key benefit that your product provides for
                     your customer- and explain the impact it can have.
                     </p>
              </div>
-             <div className="oppurtunitiesContainer">
-             <FaHandshake className="iconOppurtunitiesContainer"/>
-                    <p className="titleOppurtunitiesContainer">
+             <div className="opportunitiesContainer">
+             <FaHandshake className="iconOpportunitiesContainer"/>
+                    <p className="titleOpportunitiesContainer">
                     <b>Find best partners and suppliers</b>
                     </p>
-                    <p className="descriptionOppurtunitiesContainer">
+                    <p className="descriptionOpportunitiesContainer">
                     Describe a key benefit that your product provides for
                     your customer- and explain the impact it can have.
                     </p>
@@ -42,4 +42,4 @@ import React, {Component} from 'react';
    );
    }
   }
-export default App;
+export default Opportunities;
