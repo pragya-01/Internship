04da97d059571c6e66f8a4bc9603ad0b2beaba6c
Pragya Agrawal <56079032+pragya-01@users.noreply.github.com>
2021-06-19 06:42:36 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,47 +1,43 @@
 import './App.css';
-import { IconContext } from 'react-icons';
 import { GrNetwork} from "react-icons/gr";
 import {FaHandshake} from "react-icons/fa";
 import {GoListUnordered} from "react-icons/go";
+import React, {Component} from 'react';
 
- export class App extends React.Component{
+ export class App extends Component{
    render() {
    return (
-    <div className="Oppurtunities">
-       <IconContext.Provider
-           value={{color: '#OF1D34', size:'20%'}}
-      >
-            <div className="oppurtunitiesContainer">
-            <GrNetwork />
-            <h4>
-              Network Globally
-            </h4>
-            <p>
-              Describe a key benefit that your product provides for
-              your customer- and explain the impact it can have.
-            </p>
-            </div>
-            <div classname="oppurtunitiesContainer">
-            <GoListUnordered/>
-            <h4>
-              Enter new markets and buisness segemnts
-            </h4>
-            <p>
-              Describe a key benefit that your product provides for
-              your customer- and explain the impact it can have.
-            </p>
-            </div>
-            <div className="oppurtunitiesContainer">
-            <FaHandshake />
-            <h4>
-              Find best partners and suppliers
-            </h4>
-            <p>
-              Describe a key benefit that your product provides for
-              your customer- and explain the impact it can have.
-            </p>
-            </div>
-      </IconContext.Provider>
+   <div className="oppurtunities">
+             <div className="oppurtunitiesContainer">
+             <GrNetwork className="iconOppurtunitiesContainer"/>
+                  <p className="titleOppurtunitiesContainer">
+                   <b>Network Globally</b>
+                 </p>
+                    <p className="descriptionOppurtunitiesContainer">
+                    Describe a key benefit that your product provides for
+                    your customer- and explain the impact it can have.
+                    </p>
+             </div>
+             <div className="oppurtunitiesContainer">
+             <GoListUnordered className="iconOppurtunitiesContainer"/>
+                    <p className="titleOppurtunitiesContainer">
+                    <b>Enter new markets and buisness segments</b>
+                    </p>
+                    <p className="descriptionOppurtunitiesContainer">
+                    Describe a key benefit that your product provides for
+                    your customer- and explain the impact it can have.
+                    </p>
+             </div>
+             <div className="oppurtunitiesContainer">
+             <FaHandshake className="iconOppurtunitiesContainer"/>
+                    <p className="titleOppurtunitiesContainer">
+                    <b>Find best partners and suppliers</b>
+                    </p>
+                    <p className="descriptionOppurtunitiesContainer">
+                    Describe a key benefit that your product provides for
+                    your customer- and explain the impact it can have.
+                    </p>
+             </div>
     </div>
    );
    }
